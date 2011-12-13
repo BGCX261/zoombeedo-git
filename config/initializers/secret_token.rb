2959bf200c14b9552fc99877f3bf2cdb6393b931
Josue Basurto <josuebasurto@gmail.com>
2011-12-12 16:52:25 -0800

    JB: Se realiza cambio de tecnologia por App Engine y se crea micrositio con templates de Bootstrap

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Zoombeedo::Application.config.secret_token = '82d7023fc74b010a8d1d305a19641e79a4ab4031b60d4cd643c1578fd4899ecb45bbf78468de4466c30664b58f8d599a18fc80f08a85c4e874855bb037033c59'
