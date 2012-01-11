4a01701b785a8f7a37a4932efe0d4a59629af180
unknown <Josue@optiplex.(none)>
2012-01-10 21:42:32 -0800

    Se ajustan los css

--- main.py
+++ main.py
@@ -6,8 +6,7 @@ class MainHandler(webapp.RequestHandler):
     def get(self):
         self.response.out.write('Hello world!')
 def main():
-    application = webapp.WSGIApplication([('/', MainHandler)],
-                                         debug=True)
+    application = webapp.WSGIApplication([('/', MainHandler)],debug=True)
     util.run_wsgi_app(application)
 if __name__ == '__main__':
     main()
